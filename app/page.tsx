b600233938c2805bddcd44b570c110f3cb9eda89
Jrogeriosantos <rogesantos188@gmail.com>
2026-02-11 21:00:04 -0400

    add changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import CampaignApp from "./components/CampaignApp";
 
+export const metadata = {
+  title: "Campanha Mulher Amazônia",
+  description: "", // removed "Participe da nossa campanha especial celebrando a força e empoderamento feminino!"
+};
+
 export default function Home() {
   return <CampaignApp />;
 }
