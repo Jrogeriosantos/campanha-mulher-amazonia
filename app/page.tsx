1dfbaf01c1aa5c5509ac1d212a4c731b65267177
Jrogeriosantos <rogesantos188@gmail.com>
2026-02-11 21:22:03 -0400

    add changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,11 @@ import CampaignApp from "./components/CampaignApp";
 
 export const metadata = {
   title: "Campanha Mulher Amazônia",
-  description: "",
+  description: "Participe da nossa campanha especial em homenagem ao Dia Internacional da Mulher",
+  openGraph: {
+    title: "Campanha Mulher Amazônia",
+    description: "Participe da nossa campanha especial em homenagem ao Dia Internacional da Mulher",
+  },
 };
 
 export default function Home() {
